7b390272aa58419b48020e29af1093e0e817455a
Rizky Kurniawan <rizkykurniawan2023@gmail.com>
2022-02-27 23:29:59 +0700

    answering 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, numberOfword) {
 
+    let result = '';
+
+    if (numberOfword < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < numberOfword; i++) {
+        result += word;
+    }
+
+    return result;
 };
+
+// repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
