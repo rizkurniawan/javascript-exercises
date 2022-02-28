804b169aa794a61b4a10304f663cb951b231d385
Rizky Kurniawan <rizkykurniawan2023@gmail.com>
2022-02-28 22:33:52 +0700

    answering 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return 'ERROR';
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
 
+    let result = 0;
+
+    for (let i = a; i <= b; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
